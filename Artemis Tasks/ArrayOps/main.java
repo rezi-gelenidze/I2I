47159d0c5f338c866272ed2ec0299ae5e9071220
Rezi Gelenidze <rezi.gelenidze7@gmail.com>
2023-11-17 02:21:15 +0400

    Update main.java
    clean code

--- Artemis Tasks/ArrayOps/main.java
+++ Artemis Tasks/ArrayOps/main.java
@@ -2,53 +2,45 @@ package fop.w3simple;
 
 public class Arrays extends MiniJava {
     public static void print(int[] arr) {
-	// TODO
-    StringBuilder text = new StringBuilder("{");
-    for(int i = 0 ; i < arr.length; i++){
-        text.append(arr[i]);
-        if(i <= (arr.length-2)) {
-            text.append(", ");
+        StringBuilder text = new StringBuilder("{");
+        for (int i = 0; i < arr.length; i++) {
+            text.append(arr[i]);
+            if (i < arr.length - 1) {
+                text.append(", ");
+            }
         }
-    }
-    text.append("}");
-    writeConsole(text.toString());
+        text.append("}");
+        writeConsole(text.toString());
     }
 
     public static void minAndMax(int[] arr) {
-	// TODO
-        int Max = 0;
-        int Min = 0;
-        for(int i = 0; i < arr.length; i++) {
-            if(i == 0){
-                Max = arr[i];
-                Min = arr[i];
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
-            if(arr[i] > Max){
-                Max = arr[i];
-            }
-            if(arr[i] < Min){
-                Min = arr[i];
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        writeConsole("Minimum = " + Min +", Maximum = " + Max);
+        writeConsole("Minimum = " + min + ", Maximum = " + max);
     }
 
     public static int[] invert(int[] arr) {
-	// TODO
         int[] inverted = new int[arr.length];
-        for(int i = (arr.length-1); i >= 0; i--){
-            inverted[arr.length - i - 1] = arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            inverted[i] = arr[arr.length - i - 1];
         }
         return inverted;
     }
 
     public static int[] cut(int[] arr, int len) {
-	// TODO
         int[] newArray = new int[len];
-        for(int i = 0; i < len; i++){
-            if(i < arr.length){
+        for (int i = 0; i < len; i++) {
+            if (i < arr.length) {
                 newArray[i] = arr[i];
-            }else{
+            } else {
                 newArray[i] = 0;
             }
         }
@@ -56,15 +48,14 @@ public class Arrays extends MiniJava {
     }
 
     public static int[] linearize(int[][] arr) {
-	// TODO
         int totalElements = 0;
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             totalElements += arr[i].length;
         }
         int[] newArray = new int[totalElements];
         int index = 0;
-        for(int i = 0; i < arr.length; i++){
-            for(int a = 0; a < arr[i].length; a++){
+        for (int i = 0; i < arr.length; i++) {
+            for (int a = 0; a < arr[i].length; a++) {
                 newArray[index] = arr[i][a];
                 index++;
             }
@@ -72,12 +63,14 @@ public class Arrays extends MiniJava {
         return newArray;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] test = new int[] {-1, 2, 3, 4, 5};
-        int[][] TwoD = new int[][] {{1, 3, 5}, {-5, 0}, {5}};
-        //minAndMax(test);
-        //write(java.util.Arrays.toString(invert(test)));
-        //write(java.util.Arrays.toString(cut(test, 3)));
-        //write(java.util.Arrays.toString(linearize(TwoD)));
+        int[][] twoD = new int[][] {{1, 3, 5}, {-5, 0}, {5}};
+
+	// Example usage:
+        minAndMax(test);
+        write(java.util.Arrays.toString(invert(test)));
+        write(java.util.Arrays.toString(cut(test, 3)));
+        write(java.util.Arrays.toString(linearize(twoD)));
     }
 }
